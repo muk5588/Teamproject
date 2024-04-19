3bfbd480094cfa6005099fb584aaa5b98f7c8d8d
몰랑에른 <kdysh98@naver.com>
2024-04-19 17:38:08 +0900

    2024-04-19(네이버 로그인 제작중)

--- src/main/java/login/controller/SocialController.java
+++ src/main/java/login/controller/SocialController.java
@@ -1,20 +1,60 @@
 package login.controller;
 
-import login.service.SocialService;
+import com.github.scribejava.core.model.OAuth2AccessToken;
+import login.dto.NaverLoginVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @Controller
 public class SocialController {
     @Autowired
-    private SocialService socialService;
-
-    @RequestMapping("/login/kakaoLogin")
-    public String kakaoLogin(@RequestParam("code") String code) {
-        return null;
+    private NaverLoginVO naverLoginVO;
+    private String apiResult = null;
+    @Autowired
+    private void setnaverLoginVO(NaverLoginVO naverLoginVO) {
+        this.naverLoginVO = naverLoginVO;
     }
+    @RequestMapping(value = "/naverLogin", method = { RequestMethod.GET, RequestMethod.POST })
+    public String login(Model model, HttpSession session) {
+
+        /* 네이버아이디로 인증 URL을 생성하기 위하여 naverLoginVO클래스의 getAuthorizationUrl메소드 호출 */
+        String naverAuthUrl = naverLoginVO.getAuthorizationUrl(session);
+
+        //https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=sE***************&
+        //redirect_uri=http%3A%2F%2F211.63.89.90%3A8090%2Flogin_project%2Fcallback&state=e68c269c-5ba9-4c31-85da-54c16c658125
+        System.out.println("네이버:" + naverAuthUrl);
+
+        //네이버 
+        model.addAttribute("url", naverAuthUrl);
+
+        /* 생성한 인증 URL을 View로 전달 */
+        return "login/naverlogin";
+    }
+
+    //네이버 로그인 성공시 callback호출 메소드
+    @RequestMapping(value = "/login/navercallback", method = { RequestMethod.GET, RequestMethod.POST })
+    public String callback(Model model, @RequestParam(value = "code") String code, @RequestParam(value = "state") String state, HttpSession session)
+            throws IOException {
+        System.out.println("여기는 callback");
+        OAuth2AccessToken oauthToken;
+        oauthToken = naverLoginVO.getAccessToken(session, code, state);
+        //로그인 사용자 정보를 읽어온다.
+        apiResult = naverLoginVO.getUserProfile(oauthToken);
+        model.addAttribute("result", apiResult);
+
+        /* 네이버 로그인 성공 페이지 View 호출 */
+        return "login/naverSuccess";
+    }
+
+
+
 
 
 }
---
+++ src/main/java/login/dto/NaverLoginVO.java
@@ -0,0 +1,93 @@
+package login.dto;
+
+import com.github.scribejava.core.builder.ServiceBuilder;
+import com.github.scribejava.core.model.OAuth2AccessToken;
+import com.github.scribejava.core.model.OAuthRequest;
+import com.github.scribejava.core.model.Response;
+import com.github.scribejava.core.model.Verb;
+import com.github.scribejava.core.oauth.OAuth20Service;
+import login.Api.NaverLoginApi;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.UUID;
+
+public class NaverLoginVO {
+    private final static String CLIENT_ID = "fKMsVvHjT20ofhw2UDgN";
+    private final static String CLIENT_SECRET = "KxF0mch_Gf";
+    private final static String REDIRECT_URI = "http://localhost:8088/login/navercallback";
+    private final static String SESSION_STATE = "oauth_state";
+    /* 프로필 조회 API URL */
+    private final static String PROFILE_API_URL = "https://openapi.naver.com/v1/nid/me";
+
+    /* 네이버 아이디로 인증  URL 생성  Method */
+    public String getAuthorizationUrl(HttpSession session) {
+
+        /* 세션 유효성 검증을 위하여 난수를 생성 */
+        String state = generateRandomString();
+        /* 생성한 난수 값을 session에 저장 */
+        setSession(session,state);
+
+        /* Scribe에서 제공하는 인증 URL 생성 기능을 이용하여 네아로 인증 URL 생성 */
+        OAuth20Service oauthService = new ServiceBuilder()
+                .apiKey(CLIENT_ID)
+                .apiSecret(CLIENT_SECRET)
+                .callback(REDIRECT_URI)
+                .state(state) //앞서 생성한 난수값을 인증 URL생성시 사용함
+                .build(NaverLoginApi.instance());
+
+        return oauthService.getAuthorizationUrl();
+    }
+
+    /* 네이버아이디로 Callback 처리 및  AccessToken 획득 Method */
+    public OAuth2AccessToken getAccessToken(HttpSession session, String code, String state) throws IOException{
+
+        /* Callback으로 전달받은 세선검증용 난수값과 세션에 저장되어있는 값이 일치하는지 확인 */
+        String sessionState = getSession(session);
+        if(StringUtils.pathEquals(sessionState, state)){
+
+            OAuth20Service oauthService = new ServiceBuilder()
+                    .apiKey(CLIENT_ID)
+                    .apiSecret(CLIENT_SECRET)
+                    .callback(REDIRECT_URI)
+                    .state(state)
+                    .build(NaverLoginApi.instance());
+
+            /* Scribe에서 제공하는 AccessToken 획득 기능으로 네아로 Access Token을 획득 */
+            OAuth2AccessToken accessToken = oauthService.getAccessToken(code);
+            return accessToken;
+        }
+        return null;
+    }
+
+    /* 세션 유효성 검증을 위한 난수 생성기 */
+    private String generateRandomString() {
+        return UUID.randomUUID().toString();
+    }
+
+    /* http session에 데이터 저장 */
+    private void setSession(HttpSession session, String state){
+        session.setAttribute(SESSION_STATE, state);
+    }
+
+    /* http session에서 데이터 가져오기 */
+    private String getSession(HttpSession session){
+        return (String) session.getAttribute(SESSION_STATE);
+    }
+    /* Access Token을 이용하여 네이버 사용자 프로필 API를 호출 */
+    public String getUserProfile(OAuth2AccessToken oauthToken) throws IOException {
+
+        OAuth20Service oauthService =new ServiceBuilder()
+                .apiKey(CLIENT_ID)
+                .apiSecret(CLIENT_SECRET)
+                .callback(REDIRECT_URI).build(NaverLoginApi.instance());
+
+        OAuthRequest request = new OAuthRequest(Verb.GET, PROFILE_API_URL, oauthService);
+        oauthService.signRequest(oauthToken, request);
+        Response response = request.send();
+        return response.getBody();
+    }
+
+
+}

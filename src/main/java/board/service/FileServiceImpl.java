304685bf96bd825e4b937c342a9d5fed438892a5
neromu <jxoow153@naver.com>
2024-05-01 17:24:00 +0900

    에디터 세입( DB저장 전)

--- src/main/java/board/service/FileServiceImpl.java
+++ src/main/java/board/service/FileServiceImpl.java
@@ -1,19 +1,28 @@
 package board.service;
 
-import board.dao.FileDao;
-import board.dto.Board;
-import board.dto.BoardFile;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.ServletContext;
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-import java.util.UUID;
+import board.dao.FileDao;
+import board.dto.Board;
+import board.dto.BoardFile;
 
 @Service
 public class FileServiceImpl implements FileService {
@@ -80,6 +89,96 @@ public class FileServiceImpl implements FileService {
 	@Override
 	public List<BoardFile> getFilesByBoardNo(int boardno) {
 		return fileDao.getFilesByBoardNo(boardno);
+	}
+
+	@Override
+	public ArrayList<BoardFile> fileTempSave(HttpServletRequest request, HttpServletResponse response) {
+		
+		ArrayList<BoardFile> files = new ArrayList<BoardFile>();
+		BoardFile bFile = new BoardFile();
+		
+		String sFileInfo  = "";
+		//파일명을 받는다 - 일반 원본파일명
+		String sFilename = request.getHeader("file-name");
+		logger.debug("sFilename : {}", sFilename);
+		//파일 확장자
+		String sFilenameExt = sFilename.substring(sFilename.lastIndexOf(".")+1);
+		logger.debug("sFilenameExt : {}", sFilenameExt);
+		//확장자를소문자로 변경
+		sFilenameExt = sFilenameExt.toLowerCase();
+		logger.debug("sFilenameExt : {}", sFilenameExt);
+		try {
+			//확장자 체크를 위한 
+			String[] allowFileArr = {"jpg","png","gif"};
+			
+			//확장자 체크
+			int nCnt = 0;
+			for(int i=0; i<allowFileArr.length; i++) {
+				if(sFilenameExt.equals(allowFileArr[i])){
+					nCnt++;
+				}
+			}
+
+			//이미지가 아니라면
+			if(nCnt == 0) {
+				PrintWriter print = response.getWriter();
+				print.print("NOTALLOW_"+sFilename);
+				print.flush();
+				print.close();
+			} else {
+				//디렉토리 설정 및 업로드	
+				
+				//파일경로
+				String filePath = servletContext.getRealPath("/resources/boardUpload/");
+				logger.debug("filePath : {}", filePath);
+				File file = new File(filePath);
+				
+				if(!file.exists()) {
+					file.mkdirs();
+				}
+				
+				String sRealFileNm = "";
+//				sRealFileNm = UUID.randomUUID().toString() + sFilename.substring(sFilename.lastIndexOf("."));
+				sRealFileNm = UUID.randomUUID().toString() + sFilename;
+				String rlFileNm = filePath + sRealFileNm;
+				
+				///////////////// 서버에 파일쓰기 ///////////////// 
+				InputStream inputStream = request.getInputStream();
+				OutputStream outputStream=new FileOutputStream(rlFileNm);
+				int numRead;
+				byte bytes[] = new byte[Integer.parseInt(request.getHeader("file-size"))];
+				while((numRead = inputStream.read(bytes,0,bytes.length)) != -1){
+					outputStream.write(bytes,0,numRead);
+				}
+				if(inputStream != null) {
+					inputStream.close();
+				}
+				outputStream.flush();
+				outputStream.close();
+				
+				///////////////// 이미지 /////////////////
+				// 정보 출력
+				sFileInfo += "&bNewLine=true";
+				// img 태그의 title 속성을 원본파일명으로 적용시켜주기 위함
+				sFileInfo += "&sFileName="+ sFilename;
+				sFileInfo += "&sFileURL="+"\\resources\\boardUpload\\"+sRealFileNm;
+				PrintWriter printWriter = response.getWriter();
+				printWriter.print(sFileInfo);
+				printWriter.flush();
+				printWriter.close();
+				logger.debug("sFileInfo : {}", sFileInfo);
+				bFile.setOriginName(sFilename);
+				bFile.setStoredName(sRealFileNm);
+				logger.debug("bFile : {}", bFile);
+				files.add(bFile);
+				
+			}	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		logger.debug("files : {}", files);
+		
+		return files;
 	}
 	
 }

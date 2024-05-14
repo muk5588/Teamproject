d117e7d527962f1c364fad1642c125403e5527a3
몰랑에른 <kdysh98@naver.com>
2024-05-14 15:34:24 +0900

    2024-05-14(캘린더 링크 수정)

--- src/main/java/calendar/controller/CalendarController.java
+++ src/main/java/calendar/controller/CalendarController.java
@@ -7,11 +7,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import user.dto.User;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -28,7 +26,7 @@ public class CalendarController {
    @GetMapping("/calendar")
    public String calendarHome(){
 
-       return "/calendar/calendar";
+       return "calendar/calendar";
    }
 
    //달력 데이터 조회

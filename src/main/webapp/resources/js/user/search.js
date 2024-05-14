3aa6bef39f9b76f6a7e93bec41b0deb546b56279
몰랑에른 <kdysh98@naver.com>
2024-05-14 15:24:15 +0900

    2024-05-14(메인페이지 게시물 미리보기)

--- src/main/webapp/resources/js/user/search.js
+++ src/main/webapp/resources/js/user/search.js
@@ -4,6 +4,7 @@ window.addEventListener("DOMContentLoaded", function () {
         if (!regex.test($("#email").val())) {
             $("#alertemail").css({
                 "color": "red",
+                "text-align": "center",
                 "font-size": "15px"
             });
             $("#alertemail").text("이메일 형식이 맞지 않습니다.")
@@ -27,7 +28,7 @@ window.addEventListener("DOMContentLoaded", function () {
                     alert("서버와 통신 중 에러가 발생했습니다. ")
                     $("#alertemail").css({
                         "color": "red",
-                        "text-aline":"center",
+                        "text-align": "center",
                         "font-size": "15px"
                     });
                     $("#alertemail").text("서버와 통신 중 에러가 발생했습니다. ")
@@ -35,7 +36,7 @@ window.addEventListener("DOMContentLoaded", function () {
                     console.log("aaaa", data)
                     alert("이메일이 발송 되었습니다")
                     $("#alertemail").css({
-                        "text-aline":"center",
+                        "text-align": "center",
                         "color": "red",
                         "font-size": "15px"
                     });
@@ -50,14 +51,14 @@ window.addEventListener("DOMContentLoaded", function () {
     $("#checkcode").keyup(function () {
         if ($("#checkcode").val().length != 6) {
             $("#alertemail").css({
-                "text-aline":"center",
+                "text-align": "center",
                 "color": "red",
                 "font-size": "15px"
             });
             $("#alertemail").text("인증번호가 일치하지 않습니다")
         } else if ($("#checkcode").val() == code) {
             $("#alertemail").css({
-                "text-aline":"center",
+                "text-align": "center",
                 "color": "green",
                 "font-size": "15px"
             });
@@ -86,7 +87,7 @@ window.addEventListener("DOMContentLoaded", function () {
         var regex = new RegExp("^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[0-9a-zA-Z]{2,3}$");
         if (!regex.test($("#email2").val())) {
             $("#alertemail2").css({
-                "text-aline":"center",
+                "text-align": "center",
                 "color": "red",
                 "font-size": "15px"
             });
@@ -110,7 +111,8 @@ window.addEventListener("DOMContentLoaded", function () {
                 if (data.result == "error") {
                     alert("서버와 통신 중 에러가 발생했습니다. ")
                     $("#alertemail2").css({
-                        "text-aline":"center",
+                        "text-align": "center",
+
                         "color": "red",
                         "font-size": "15px"
                     });
@@ -119,7 +121,7 @@ window.addEventListener("DOMContentLoaded", function () {
                     console.log("aaaa", data)
                     alert("이메일이 발송 되었습니다")
                     $("#alertemail2").css({
-                        "text-aline":"center",
+                        "text-align": "center",
                         "color": "red",
                         "font-size": "15px"
                     });
@@ -135,13 +137,15 @@ window.addEventListener("DOMContentLoaded", function () {
         if ($("#checkcode2").val().length != 6) {
             $("#alertemail2").css({
                 "color": "red",
-                "text-aline":"center",
+                "text-align": "center",
+
                 "font-size": "15px"
             });
             $("#alertemail2").text("인증번호가 일치하지 않습니다")
         } else if ($("#checkcode2").val() == code) {
             $("#alertemail2").css({
-                "text-aline":"center",
+                "text-align": "center",
+
                 "color": "green",
                 "font-size": "15px"
             });

3fda32410e85f9ce0d0ac64549cf2d30a31fd288
몰랑에른 <kdysh98@naver.com>
2024-05-16 15:44:36 +0900

    2024-05-16(날씨 주소 변경)

--- src/main/java/util/Weather.java
+++ src/main/java/util/Weather.java
@@ -30,7 +30,7 @@ public class Weather {
         if (user == null) {
             x = "126.98322015";
             y = "37.57023746";
-            address = "서울특별시 종로1가";
+            address = "서울시 중구 종로1가";
             model.addAttribute("x",x);
             model.addAttribute("y",y);
             model.addAttribute("address",address);
@@ -39,10 +39,11 @@ public class Weather {
             HashMap<String, String> XYMap = geoCoding.getXYMapfromJson(geoCoding.getKakaoApiFromAddress(address));
             x = XYMap.get("x");   //위도
             y = XYMap.get("y");   //경도
-            String address1 = XYMap.get("address");
+            String[] addressParts = address.split(" ");
+            address = (addressParts.length >= 3) ? addressParts[0] + " " + addressParts[1] + " " + addressParts[2]: address;
             model.addAttribute("x",x);
             model.addAttribute("y",y);
-            model.addAttribute("address",address1);
+            model.addAttribute("address",address);
         }
 
         return "layout/weather";

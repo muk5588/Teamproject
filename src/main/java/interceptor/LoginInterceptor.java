5db7a73e4bcba432b3b72571b9c410647d190823
neromu <jxoow153@naver.com>
2024-05-28 11:17:33 +0900

    인터셉터 기본 설정

--- src/main/java/interceptor/LoginInterceptor.java
+++ src/main/java/interceptor/LoginInterceptor.java
@@ -1,17 +1,31 @@
 package interceptor;
 
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import user.dto.User;
+
 @Component
 public class LoginInterceptor implements HandlerInterceptor{
-	//인터셉터 패키지 실행전 AOP로 로그인 검사 진행되었음.
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
+		HttpSession session = request.getSession();
+		User user = (User) session.getAttribute("dto1");
+		if(user == null ) {
+    	   session.invalidate();
+           PrintWriter out = response.getWriter();
+//           out.println("<script>alert('로그인상태가 아닙니다');</script>");
+           out.println("<script>window.location.href='/login';</script>");
+           out.flush();
+           return false;
+       }
 		return true;
 	}
 	

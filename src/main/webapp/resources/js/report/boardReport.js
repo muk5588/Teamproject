52ff5540d8175f549a57dff969649a58451019ce
neromu <jxoow153@naver.com>
2024-05-30 11:25:38 +0900

    1.board 신고 들어가면 게시글개수 적게보이는거 - 해결 2.신고이후 이동 페이지 URL 수정 - 해결 3.게시글 상세보기에서 목록으로 -categoryNo 없음 - 이전페이지 이동으로 변경 - 해결 4.신고하기 alert 말고 confirm 처리 - 해결 4-1. report/commentReport -해결 4-2. report/boardReport - 해결

--- src/main/webapp/resources/js/report/boardReport.js
+++ src/main/webapp/resources/js/report/boardReport.js
@@ -1,18 +1,21 @@
-window.addEventListener("DOMContentLoaded",function () {
+window.addEventListener("DOMContentLoaded", function () {
     $("#report").click(function () {
-        var val = $("#reportContent").val().trim()
+        var val = $("#reportContent").val().trim();
         if ($("#reportContent").val().length === 0) {
-            alert("내용을 입력해주세요")
-            $("#reportContent").focus()
+            alert("내용을 입력해주세요");
+            $("#reportContent").focus();
             return false;
-        }else if(val.replace(/\s| /gi,"").length == 0){
+        } else if (val.replace(/\s| /gi, "").length == 0) {
             alert('내용이 공백만 입력되었습니다 ');
-            $("#reportContent").focus()
+            $("#reportContent").focus();
             return false;
         } else {
-            alert("신고하시겠습니까?")
-            $("#reportform").submit();
-            alert("신고 완료 되었습니다")
+            if (confirm("신고하시겠습니까?")) {
+                $("#reportform").submit();
+                alert("신고 완료 되었습니다");
+            } else {
+                alert("신고가 취소되었습니다");
+            }
         }
-    })
+    });
 });
